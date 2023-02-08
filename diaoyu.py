450603a08225a86b14cfaa3f283f5211c5c1a167
zzy <zzyzhouzhiying@outlook.com>
2023-02-08 16:37:26 +0900

    2个鱼漂时候用IOU

--- diaoyu.py
+++ diaoyu.py
@@ -17,6 +17,35 @@ from yolox.utils import mkdir, multiclass_nms, demo_postprocess, vis
 from pyHM import mouse
 
 pyautogui.FAILSAFE = True
+def compute_iou(rec_1,rec_2):
+    '''
+    rec_1:左上角(rec_1[0],rec_1[1])    右下角：(rec_1[2],rec_1[3])
+    rec_2:左上角(rec_2[0],rec_2[1])    右下角：(rec_2[2],rec_2[3])
+
+    '''
+    rec_1[0]=int(rec_1[0])
+    rec_1[1]=int(rec_1[1])
+    rec_1[2]=int(rec_1[2])
+    rec_1[3]=int(rec_1[3])
+    rec_2[0]=int(rec_2[0])
+    rec_2[1]=int(rec_2[1])
+    rec_2[2]=int(rec_2[2])
+    rec_2[3]=int(rec_2[3])
+
+    s_rec1=(rec_1[2]-rec_1[0])*(rec_1[3]-rec_1[1])   #第一个bbox面积 = 长×宽
+    s_rec2=(rec_2[2]-rec_2[0])*(rec_2[3]-rec_2[1])   #第二个bbox面积 = 长×宽
+    sum_s=s_rec1+s_rec2                              #总面积
+    left=max(rec_1[0],rec_2[0])                      #并集左上角顶点横坐标
+    right=min(rec_1[2],rec_2[2])                     #并集右下角顶点横坐标
+    bottom=max(rec_1[1],rec_2[1])                    #并集左上角顶点纵坐标
+    top=min(rec_1[3],rec_2[3])                       #并集右下角顶点纵坐标
+    if left >= right or top <= bottom:               #不存在并集的情况
+        return 0
+    else:
+        inter=(right-left)*(top-bottom)              #求并集面积
+        iou=(inter/(sum_s-inter))*1.0                #计算IOU
+        return iou
+
 def yupiaoshibie(imgpath):
     input_shape = tuple(map(int, ['640', '640']))
     origin_img = cv2.imread(imgpath)
@@ -51,12 +80,10 @@ def yupiaoshibie(imgpath):
                 box.append(final_boxes[count])
                 score.append(final_scores[count])
             count=count+1
-    if len(box)>1 or len(score)>1:
-        print("error")
-        return None,None
-    return box,score
+    return len(box),box,score
 
 if __name__ == '__main__':
+    jilu=[]
     try:
         print('请输入a开始')
         keyboard.wait('a')
@@ -66,23 +93,71 @@ if __name__ == '__main__':
             quyu = (420,0,1500,800)
             img = ImageGrab.grab(quyu)
             img.save('yu.png')
-            box,score=yupiaoshibie('./yu.png')    
+            lenn,box,score=yupiaoshibie('./yu.png')    
             # box,score=yupiaoshibie('./assets/a67.png')
-            x1=box[0][0]
-            y1=box[0][1]
-            x2=box[0][2]
-            y2=box[0][3]
-            x=random.randint(int(x1),int(x2))
-            y=random.randint(int(y1),int(y2))
-            x=int(1920*x/1080)
-            y=int(1080*x/800)
-            speed=random.uniform(0.5,1.5)
-            mouse.move(x, y, multiplier=speed)
-            # pyautogui.moveTo(x, y, speed, pyautogui.easeInOutQuad)
-            keyboard.wait('space')
-            t=random.uniform(1,1.5)
-            print('sleep',t)
-            time.sleep(t)
+            if(lenn==1):
+                print(box)
+                image = cv2.imread('yu.png')
+                cv2.rectangle(image, (int(box[0][0]), int(box[0][1])), (int(box[0][2]), int(box[0][3])), (0, 0, 255), 2)  
+                cv2.imwrite('1.png', image)
+                jilu.append(box[0])
+                x1=box[0][0]
+                y1=box[0][1]
+                x2=box[0][2]
+                y2=box[0][3]
+                x=random.randint(int(x1),int(x2))
+                y=random.randint(int(y1),int(y2))
+                x=x+420
+                speed=random.uniform(0.5,1.5)
+                mouse.move(x, y, multiplier=speed)
+                # pyautogui.moveTo(x, y, speed, pyautogui.easeInOutQuad) 
+                keyboard.wait('space')
+                t=random.uniform(1,1.5)
+                time.sleep(t)
+            if(lenn==2):
+                print(box)
+                #此处分别计算第二次截图中两个鱼漂box和第一次鱼漂box的iou，iou大的即为第一次的鱼漂坐标，则选iou小的那个作为输出并保留其box（放进jilu里取代之前的）
+                image = cv2.imread('yu.png')
+                cv2.rectangle(image, (int(box[0][0]), int(box[0][1])), (int(box[0][2]), int(box[0][3])), (0, 0, 255), 2)  
+                cv2.rectangle(image, (int(box[1][0]), int(box[1][1])), (int(box[1][2]), int(box[1][3])), (0, 0, 255), 2)  
+                cv2.imwrite('1.png', image)
+                i0=compute_iou(box[0],jilu[0])
+                i1=compute_iou(box[1],jilu[0])
+                jilu.pop(0)
+                if(i0<i1):
+                    jilu.append(box[0])
+                    x1=box[0][0]
+                    y1=box[0][1]
+                    x2=box[0][2]
+                    y2=box[0][3]
+                    x=random.randint(int(x1),int(x2))
+                    y=random.randint(int(y1),int(y2))
+                    x=x+420
+                    speed=random.uniform(0.5,1.5)
+                    mouse.move(x, y, multiplier=speed)
+                    # pyautogui.moveTo(x, y, speed, pyautogui.easeInOutQuad) 
+                    keyboard.wait('space')
+                    t=random.uniform(1,1.5)
+                    time.sleep(t)
+                else:
+                    jilu.append(box[1])
+                    x1=box[1][0]
+                    y1=box[1][1]
+                    x2=box[1][2]
+                    y2=box[1][3]
+                    x=random.randint(int(x1),int(x2))
+                    y=random.randint(int(y1),int(y2))
+                    x=x+420
+                    speed=random.uniform(0.5,1.5)
+                    mouse.move(x, y, multiplier=speed)
+                    # pyautogui.moveTo(x, y, speed, pyautogui.easeInOutQuad) 
+                    keyboard.wait('space')
+                    t=random.uniform(1,1.5)
+                    time.sleep(t)
+            else:
+                print('gg')
+                keyboard.wait('space')
+
     except SyntaxError:
         print('error')
         pass
